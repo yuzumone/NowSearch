b278fd70ebe04fa757c483613d8df061b17beb7e
yuzumone <yuzumone@outlook.com>
2018-07-08 09:23:21 +0900

    Add dark mode

--- app/src/main/java/net/yuzumone/nowsearch/MainActivity.kt
+++ app/src/main/java/net/yuzumone/nowsearch/MainActivity.kt
@@ -2,11 +2,15 @@ package net.yuzumone.nowsearch
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import com.twitter.sdk.android.core.Twitter
 import com.twitter.sdk.android.core.TwitterAuthConfig
 import com.twitter.sdk.android.core.TwitterConfig
 
 class MainActivity : AppCompatActivity() {
+
+    private val setting by lazy { Setting(this) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,5 +24,24 @@ class MainActivity : AppCompatActivity() {
             supportFragmentManager.beginTransaction()
                     .add(android.R.id.content, SearchFragment()).commit()
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        menu.findItem(R.id.menu_dark).isChecked = setting.darkMode
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id = item.itemId
+        if (id == R.id.menu_dark) {
+            item.isChecked = !item.isChecked
+            setting.darkMode = item.isChecked
+            val intent = intent
+            finish()
+            startActivity(intent)
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
